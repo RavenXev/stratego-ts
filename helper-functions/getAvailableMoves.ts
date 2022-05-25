d5ffe6115277aafe8f4969e3726f36fb8f4134fb
Evan X <64918172+RavenXev@users.noreply.github.com>
2022-05-25 11:07:18 -0400

    implemented scout

--- helper-functions/getAvailableMoves.ts
+++ helper-functions/getAvailableMoves.ts
@@ -16,33 +16,118 @@ function getAvailableMoves(
   let anticolor: Piece["color"];
   if (color === "red") {
     anticolor = "blue";
-  } else if (color === "blue") {
+  }
+  if (color === "blue") {
     anticolor = "red";
   }
   if (rank === 0 || rank === 99 || rank === null || rank === -1) {
     return [];
   }
 
+  if (rank === 2) {
+    let validMoves: number[] = [];
+
+    let currentPosition = position + 1;
+    while (Math.floor(currentPosition / 10) == Math.floor(position / 10)) {
+      if (gameState[currentPosition].rank == null) {
+        validMoves.push(currentPosition);
+      } else if (
+        gameState[currentPosition].color == color ||
+        gameState[currentPosition].rank === -1
+      ) {
+        break;
+      } else if (color == "red" && gameState[currentPosition].color == "blue") {
+        validMoves.push(currentPosition);
+        break;
+      } else if (color == "blue" && gameState[currentPosition].color == "red") {
+        validMoves.push(currentPosition);
+        break;
+      }
+      currentPosition++;
+    }
+
+    currentPosition = position - 1;
+    while (Math.floor(currentPosition / 10) == Math.floor(position / 10)) {
+      if (gameState[currentPosition].rank == null) {
+        validMoves.push(currentPosition);
+      } else if (
+        gameState[currentPosition].color == color ||
+        gameState[currentPosition].rank === -1
+      ) {
+        break;
+      } else if (color == "red" && gameState[currentPosition].color == "blue") {
+        validMoves.push(currentPosition);
+        break;
+      } else if (color == "blue" && gameState[currentPosition].color == "red") {
+        validMoves.push(currentPosition);
+        break;
+      }
+      currentPosition--;
+    }
+
+    currentPosition = position + 10;
+    while (currentPosition <= 99) {
+      if (gameState[currentPosition].rank == null) {
+        validMoves.push(currentPosition);
+      } else if (
+        gameState[currentPosition].color == color ||
+        gameState[currentPosition].rank === -1
+      ) {
+        break;
+      } else if (color == "red" && gameState[currentPosition].color == "blue") {
+        validMoves.push(currentPosition);
+        break;
+      } else if (color == "blue" && gameState[currentPosition].color == "red") {
+        validMoves.push(currentPosition);
+        break;
+      }
+      currentPosition += 10;
+    }
+
+    currentPosition = position - 10;
+    while (currentPosition >= 0) {
+      if (gameState[currentPosition].rank == null) {
+        validMoves.push(currentPosition);
+      } else if (
+        gameState[currentPosition].color == color ||
+        gameState[currentPosition].rank === -1
+      ) {
+        break;
+      } else if (color == "red" && gameState[currentPosition].color == "blue") {
+        validMoves.push(currentPosition);
+        break;
+      } else if (color == "blue" && gameState[currentPosition].color == "red") {
+        validMoves.push(currentPosition);
+        break;
+      }
+      currentPosition -= 10;
+    }
+    return validMoves;
+  }
   // check if the piece is on the right or left edge of the board
   // AKA if the piece is on 0,10,20,30,40,50,60,70,80,90
   // or 9,19,29,39,49,59,69,79,89,99.
   // for instance, a piece on 9 cannot move to 10 and vice versa.
 
   // everything else but a 2
-  let validMoves = [position + 1, position - 1, position + 10, position - 10];
-  if (position % 10 === 9) {
-    validMoves = [position - 1, position + 10, position - 10];
-  } else if (position % 10 === 0) {
-    validMoves = [position + 1, position + 10, position - 10];
-  }
-  validMoves = validMoves.filter((position) => position >= 0 && position <= 99);
-  validMoves = validMoves.filter(
-    (currentPosition) =>
-      gameState[currentPosition].rank == null ||
-      gameState[currentPosition].color == anticolor
-  );
+  else {
+    let validMoves = [position + 1, position - 1, position + 10, position - 10];
+    if (position % 10 === 9) {
+      validMoves = [position - 1, position + 10, position - 10];
+    } else if (position % 10 === 0) {
+      validMoves = [position + 1, position + 10, position - 10];
+    }
+    validMoves = validMoves.filter(
+      (position) => position >= 0 && position <= 99
+    );
+    validMoves = validMoves.filter(
+      (currentPosition) =>
+        gameState[currentPosition].rank == null ||
+        gameState[currentPosition].color == anticolor
+    );
 
-  return validMoves;
+    return validMoves;
+  }
 }
 
 export default getAvailableMoves;
